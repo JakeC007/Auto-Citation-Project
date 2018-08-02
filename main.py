ede79b3cb42c23c4e82838dd2ca55fadeee4fb52
Jake C <36495719+JakeC007@users.noreply.github.com>
2018-08-02 15:52:08 -0700

    Added the storage function
    working now

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 #!/bin/env python3
-
+#Created by Jake Chanenson 8/1/18. 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from shelf_g import *
 
+#List of Sample ISBN Numbers
+#isbn = '0743297334' TSOR
+#isbn = '0788789813' LOTR
+#isbn = '0743273567' Gats
+#isbn = '0684801221' OMAS
+#isbn = '0684803356' For who the bell tolls 
+#isbn = '1841499889' The expanse book 1
+#isbn = '1503261964' Emma
+#isbn = '0060850523' BNW
+#isbn = '0399501487' LoF
+#isbn = '185326086X' Dracula
+#isbn = '0141439475' Frankenstien 
+
 
 def main():
-  x = 0
-  BS= ["one", "two", "three"]
+  print("Welcome to the citation generator. This program prints MLA style citations if you input an ISBN. Type 'bib' if you want a list of all stored entries. Type 'stop' to quit.\n"+"*"*100)
+
+  BS= []
   while True:
-    #isbn = '0136108040'
-    isbn = input("Enter your ISBN: ")
-    
-    if isbn == 'stop':
+    isbn = input("\n Enter your ISBN: ")
+    if isbn == "bib":
+      BS.sort(key=lambda book: (book.author, book.title))
+      print("\n Bibliography\n" + "-"*90)
+      for i in range(len(BS)):
+        print(BS[i])
+      print("-"*90)
+    elif isbn == "stop":
       break
-
-    f = urlopen('https://www.ottobib.com/isbn/%s/bibtex' % (isbn))
-    html = f.read()
-    soup = bs(html, 'html.parser')
-    bibtex = soup.find('textarea').text
-        
-    str5 = parse(bibtex)
-    
-    #Nonse to try and store multiple enteries
-    x +=1
-    if x == 1:
-      book1 = book(str5[0], str5[1], str5[2], str5[3], str5[4], str5[5])
-      BS[0] = book1
+    else:
+      f = urlopen('https://www.ottobib.com/isbn/%s/bibtex' % (isbn))
+      html = f.read()
+      soup = bs(html, 'html.parser')
+      bibtex = soup.find('textarea').text
+      str5 = parse(bibtex)
+      n_book = book(str5[0], str5[1], str5[2], str5[3], str5[4], str5[5])
+      print(n_book)
+      BS.append(n_book)
       
-    elif x == 2:
-      book2 = book(str5[0], str5[1], str5[2], str5[3], str5[4], str5[5])
-      BS[1] = book2
-    
-    elif x == 3:
-      book3 = book(str5[0], str5[1], str5[2], str5[3], str5[4], str5[5])
-      BS[2] = book3
-    
-  
-  for j in range(x):
-    print(BS[j])
-  
-
   
     
 
@@ -54,6 +55,7 @@ def parse(string):
   str5 = str4
   for i in range (6):
     str5[i] = str4[i].strip('{ },') #strip the brackets 
+  str5[1] = str5[1].title()
   return str5 
 
 main()
